58224d142bd3dab96664b656f84c95865875473e
lok <winglok1998@gmail.com>
2026-03-16 16:22:37 +0800

    merge

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -1,11 +1,20 @@
-import { Button } from "@/app/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const dashboard = () => {
-  return <>
-  <Button>
-    
-</Button>
-  </>;
+    return (
+        <>
+            {/* Small size */}
+            <Button size="sm">Small Button</Button>
+
+            {/* Default size */}
+            <Button size="default">Default Button</Button>
+
+            {/* Large size */}
+            <Button size="lg">Large Button</Button>
+
+            <Button className={`h-50`}>tailwindcss</Button>
+        </>
+    );
 };
 
 export default dashboard;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-
+import path from "node:path"
 
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()], 
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./app"),
+    },
+  },
 });
